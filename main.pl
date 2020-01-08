0ba30ced78d45f2f909fde805c28e6de49d024c1
Puchacz007 <mateyou2@gmail.com>
2020-01-08 22:18:40 +0100

    basic tk ui added for needed options

--- main.pl
+++ main.pl
@@ -14,7 +14,7 @@ my @lines = load_records(USERS_DB);
     my $lb    = $mw->Listbox(
         -relief  => 'sunken',
         -height  => 5,
-        -width   => 100,
+        -width   => 50,
         -setgrid => 1,
     )->pack( -side => 'left', -padx => 1 );
     foreach (@lines) {
@@ -37,11 +37,44 @@ my $button1 = $mw->Button(
         newUser($top);
          }
     }
-)->pack( -padx => 75, -pady => 5 );
+)->pack( -padx => 10, -pady => 5 );
+my $button2 = $mw->Button(
+    -text    => 'Modify user groups',
+    -command => sub {
+       
+       if ( !Exists($top) ) {
+           $top = $mw->Toplevel;
+        modifyUserGroups($top);
+         }
+    }
+    )->pack( -padx => 10, -pady => 5 );
+  my $button3 = $mw->Button(
+    -text    => "Copy file to home catalogue",
+    -command => sub {
+       
+    if ( !Exists($top) ) {
+           $top = $mw->Toplevel;
+        copyFile($top);
+         }
+         }
+    
+     
+    
+    )->pack( -padx => 10, -pady => 5 );
+my $button4 = $mw->Button(
+    -text    => 'Delete user',
+    -command => sub {
+       
+       if ( !Exists($top) ) {
+           $top = $mw->Toplevel;
+        
+         }
+    }
+)->pack( -padx => 10, -pady => 5 );
 my $quit = $mw->Button(
     -text    => 'Quit',
     -command => sub { exit },
-)->pack( -padx => 75, -pady => 5 );
+)->pack( -padx => 10, -pady => 5 );
 MainLoop;
 
 sub newUser {
@@ -52,23 +85,49 @@ sub newUser {
     my $uid = $top->Entry()->pack( -padx => 40 );
     $top->Label( -text => "Please write down your login" )->pack( -padx => 40 );
     my $login = $top->Entry()->pack( -padx => 40 );
-    $top->Label( -text => "Please write down your password\nor leave empty to generate it" )
+    $top->Label( -text => "Please write down your password" )
       ->pack( -padx => 40 );
-    my $password = $top->Entry()->pack( -padx => 40 );
-    $button1 = $top->Button(
+      my $password="";
+    my $passwordEntry = $top->Entry()->pack( -padx => 40 );
+    
+    
+    
+    
+    
+    
+     $button1 = $top->Button(
+        -text    => 'Generate password',
+        -command => sub {
+		$password="";
+	for (my $i=0; $i <= 9; $i++)
+        {
+		 my @generator = ('a'..'z','A'..'Z','0'..'9');
+		
+		$password =join("",$password,$generator[int rand @generator]);
+		
+		}    
+		$passwordEntry->configure(-textvariable =>$password );
+        }
+    )->pack( -padx => 40, -pady => 5 );
+    
+    
+    
+    
+    
+    $top->Button(
         -text    => 'Save user',
         -command => sub {
-	if(add_new_record(USERS_DB,$uid->get,$login->get,$password->get))
+	if(add_new_record(USERS_DB,$uid->get,$login->get,$password) && $password ne "" && $login->get ne "")
             {
             $top->destroy;
     }else
     {
            my  $errorMessage = $top->Toplevel;
-               $errorMessage->Label( -text => "Not Unique UID" )->pack( -padx => 40 );
+               $errorMessage->Label( -text => "Not Unique UID or no password or no login" )->pack( -padx => 40 );
                my $close= $errorMessage->Button(
     -text    => 'close',
     -command => sub {$errorMessage->destroy; },
-)->pack( -padx => 75, -pady => 5 );
+)->pack( -padx => 40, -pady => 5 );
 	    }
         }
     )->pack( -padx => 40, -pady => 5 );
@@ -80,6 +139,89 @@ sub saveNewUser{
     
     
     }
+    
+    
+    
+    sub modifyUserGroups{
+	     my ($top) = @_;
+	    
+	    	    
+	    	    my $top2;
+	    	      my $topLb    = $top->Listbox(
+        -relief  => 'sunken',
+        -height  => 5,
+        -width   => 20,
+        -setgrid => 1,
+    )->pack( -side => 'left', -padx => 1 );
+    foreach (@lines) {
+       $topLb->insert( 'end', $_ );
+
+    }
+    my $TopScroll = $top->Scrollbar( -command => [ 'yview', $topLb ] );
+   $topLb->configure( -yscrollcommand => [ 'set',$TopScroll] );
+    $topLb->pack( -side => 'left', -fill => 'both', -expand => 1 );
+   $TopScroll->pack( -side => 'left', -fill => 'y' );
+	    	    
+	 
+	 my $button1 = $top->Button(
+    -text    => 'Add new user group',
+    -command => sub {
+       
+       if ( !Exists($top2) ) {
+           $top2 = $top->Toplevel;
+        newUserGroup($top2);
+         }
+    }
+)->pack( -padx => 10, -pady => 5 );   	
+my $button2 = $top->Button(
+    -text    => 'Delete group',
+    -command => sub {
+       
+    }
+)->pack( -padx => 10, -pady => 5 );   	   
+	    }
+    
+ sub   newUserGroup{
+	     my ($top) = @_;
+	     $top->Label( -text => "Please write down new user group" )->pack( -padx => 40 );
+    my $userGroup = $top->Entry()->pack( -padx => 40 );
+   
+	    
+	  $button1 = $top->Button(
+        -text    => 'Save user group',
+        -command => sub {
+	
+            
+            $top->destroy;
+    
+	    })->pack( -padx => 40, -pady => 5 );
+    }
+    
+    
+    
+    sub copyFile{
+	    my ($top) = @_;
+	    $top->Label( -text => "Please write down file adress to copy file from" )->pack( -padx => 40 );
+	      my $fileToCopy = $top->Entry()->pack( -padx => 40 );
+	    $button1 = $top->Button(
+        -text    => 'Apply',
+        -command => sub {
+	
+            
+            $top->destroy;
+    
+	    })->pack( -padx => 40, -pady => 5 );
+    }
+	    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     sub new_db {
     my ($db) = @_;
     if ( not -f $db ) {
@@ -90,6 +232,7 @@ sub saveNewUser{
     }
     return (1);
 }
+
 
 sub add_new_record {
     my ( $db, $uid,$login,$password  ) = @_;
@@ -127,15 +270,6 @@ sub add_new_record {
 			   }
 		   }
 }
-if($password eq "")
-   {
-      for (my $i=0; $i <= 9; $i++)
-        {
-		 my @generator = ('a'..'z','A'..'Z','0'..'9');
-		
-		$password =join("",$password,$generator[int rand @generator]);
-		}    
-   }
   $password = sha512_base64($password);
    my $record;
        $record = join( '=>', $uid,$login,$password);  
